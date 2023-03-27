b3e7d4ae20cf212a90e77155df89d575d0fcdcda
JoseAVallejo12 <josealfredovallejo25@gmail.com>
2023-03-27 18:45:33 -0500

    add tailwindcss and postcss

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -53,24 +53,82 @@ export const LoginPage = () => {
   };
 
   return (
-    <div className="login-page">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h1>Iniciar sesión</h1>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    // <div className="login-page">
+    //   <form className="login-form" onSubmit={handleSubmit}>
+    //     <h1>Iniciar sesión</h1>
+    //     <div className="form-group">
+    //       <label htmlFor="email">Email</label>
+    //       <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+    //     </div>
+    //     <div className="form-group">
+    //       <label htmlFor="password">Contraseña</label>
+    //       <div className="password-input">
+    //         <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+    //         <i className="far fa-eye" onClick={handlePasswordVisibility}></i>
+    //       </div>
+    //     </div>
+    //     <button type="submit" className="btn-login" disabled={loading}>
+    //       {loading ? "Cargando..." : "Ingresar"}
+    //     </button>
+    //   </form>
+    // </div>
+    <div className="flex items-center justify-center min-h-screen">
+    <div className="w-full max-w-md">
+      <form
+        // onSubmit={handleLogin}
+        className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Enter your username"
+            // value={username}
+            // onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Contraseña</label>
-          <div className="password-input">
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            <i className="far fa-eye" onClick={handlePasswordVisibility}></i>
-          </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button type="submit" className="btn-login" disabled={loading}>
-          {loading ? "Cargando..." : "Ingresar"}
-        </button>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
       </form>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2023 Acme Corporation. All rights reserved.
+      </p>
     </div>
+  </div>
   );
 };
